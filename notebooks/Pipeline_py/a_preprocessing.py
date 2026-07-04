1b945aef2de887e6c04c719c57a0a8a7bf04b4c4
Adam Broomhead <alb25@apollo01.ma.ic.ac.uk>
2026-07-04 00:46:56 +0000

    progress on creating runner

---
+++ notebooks/Pipeline_py/a_preprocessing.py
@@ -0,0 +1,119 @@
+import polars as pl 
+import utils as ut
+
+config_dict = ut.load_json5('config_dict')
+('/home/ma/a/alb25/Project/thesis_code/notebooks/configs/config_dict.json')
+
+# Helper functions outputing dicitonaries
+def get_bin_metrics(config_dict=config_dict):
+    ''' 
+    Returns a dictionary containing all information needed about bin length these numbers are needed throughout the pipeline
+    '''
+    # Check that bin length divides number of days
+    assert 24 * 60 % config_dict['fine_bin_mins'] == 0
+    assert 24 * 60 % config_dict['coarse_bin_mins'] == 0
+    assert config_dict['coarse_bin_mins'] % config_dict['fine_bin_mins'] == 0
+
+    output_dict = {'fine_bins_per_day' : 24 * 60 // config_dict['fine_bin_mins']}
+    output_dict['fine_bins_per_week'] = output_dict['fine_bins_per_day'] * 7
+    output_dict['fine_bins_per_coarse_bin'] = config_dict['coarse_bin_mins'] // config_dict['fine_bin_mins']
+    output_dict['coarse_bins_per_week'] = output_dict['fine_bins_per_week'] // output_dict['fine_bins_per_coarse_bin']
+    output_dict['fine_bin_seconds'] = config_dict['fine_bin_mins'] * 60 
+
+    return output_dict
+
+def get_train_test_split(config_dict=config_dict):
+    ''' 
+    Calculates the fine bin index of the train test and validation period start and end
+    Returns a dictionary with all this information in
+    '''
+
+    fine_bins_per_day = 24 * 60 // config_dict['fine_bin_mins']
+
+    train_bins = fine_bins_per_day * config_dict['train_days']
+    burn_in_bins = fine_bins_per_day * config_dict['burn_in_days']
+    validation_bins = fine_bins_per_day * config_dict['validation_days']
+    test_bins = fine_bins_per_day * config_dict['test_days']
+
+    # Constructing a dict for the output
+    output_dict = {'train_start' : 0,
+                   'train_end' : train_bins}
+    
+    output_dict['burn_in_start'] = output_dict['train_end']
+    output_dict['burn_in_end'] = output_dict['burn_in_start'] + burn_in_bins
+
+    output_dict['validation_start'] = output_dict['burn_in_end']
+    output_dict['validation_end'] = output_dict['validation_start'] + validation_bins
+
+    output_dict['test_start'] = output_dict['validation_end']
+    output_dict['test_end'] = output_dict['test_start'] + test_bins
+    
+    return output_dict
+    
+# Adding the fine_bins_per_coarse_bin to the train_test_split_dict
+def add_training_denom(bin_metric_dict, config_dict=config_dict):
+    ''' 
+    Finds how many fine bins are used for the estimate of the iniatial parameter.
+    This is the denominator on the mean estimate as it is counts/bins and is similarly used in the variance estimate.
+
+    This fucntion assumes train days is a multiple of 7 to work
+
+    Returns:
+        An updated bin metric dict with a new column train denom and cluster denom
+    '''
+    assert config_dict['train_days'] % 7 == 0
+    bin_metric_dict['train_denom'] = bin_metric_dict['fine_bins_per_coarse_bin'] * (config_dict['train_days'] // 7)
+    return bin_metric_dict
+
+
+####
+
+# Creating a fine bin and identifying users with some counts in the bin
+
+def create_counts_data(df):
+    ''' 
+    Gets the counts per fine bin x source user from the data
+    '''
+    df = df.with_columns(time = pl.col('time').dt.total_seconds())
+    df = df.with_columns(fine_bin_id = pl.col('time') // bin_metric_dict['fine_bin_seconds'])
+    user_x_fine_bin_cnts = df.group_by(['source_user@domain', 'fine_bin_id']).agg(pl.len().alias('count')).collect()
+    return user_x_fine_bin_cnts
+
+def create_user_to_id_mapping(users_df, mapping_file_name):
+        ''' 
+        Creates a table containing the 
+        '''
+        # Creating a user lookup table and storing it
+        user_mapping = users_df.select('source_user@domain').unique().sort(by='source_user@domain').with_row_index('user_id')
+        user_mapping = user_mapping.with_columns(source_user_type = pl.when(pl.col('source_user@domain').str.contains(r"^U\d+@")).then(pl.lit("human")
+                                                ).when(pl.col('source_user@domain').str.contains(r"^C\d+\$@")).then(pl.lit("machine")))
+        store_data(user_mapping, mapping_file_name)
+        
+        # Joining on the lookup table and dropping columns
+        users_df = users_df.join(user_mapping, on='source_user@domain', how='inner')
+        users_df = users_df.select(['user_id', 'fine_bin_id', 'count']).sort(['user_id', 'fine_bin_id'])
+
+        return users_df, user_mapping
+
+def create_coarse_bins(users_df):
+        ''' 
+        takes a DF and creates two new columns:
+            coarse_bin_id
+            fine_bin_within_coarse_pos
+        '''
+        # Creating columns needed fr
+        users_df = users_df.with_columns(fine_bin_pos_in_week = pl.col('fine_bin_id') % bin_metric_dict['fine_bins_per_week'])
+        users_df = users_df.with_columns(coarse_bin_id = pl.col('fine_bin_pos_in_week') // bin_metric_dict['fine_bins_per_coarse_bin'])
+        return users_df
+
+def create_first_last_interaction_arrays(user_counts):
+    '''
+    Creates an output table with user first and last interaction indicies within df counts
+    Note this is not a fine bin index but a row index
+    '''
+    # Creating a lookup table for numba for the first and last entry of users 
+    user_interactions = user_counts.with_row_index().group_by('user_id').agg(
+        user_first_index = pl.min('index'),
+        user_last_index = pl.max('index')).sort(by='user_id')
+    
+    return user_interactions
---
+++ notebooks/Pipeline_py/utils.py
@@ -0,0 +1,45 @@
+import polars as pl 
+import json5
+
+data_path = '/home/ma/a/alb25/Project/thesis_code/data/processed/intermediate'
+input_data = 'train_df'
+
+# Creating functions for storing and reading data
+def store_data(data, filename, data_path=data_path, csv=False):
+    if isinstance(data, pl.LazyFrame):
+        if csv == True:
+            data.sink_csv(f'{data_path}/output/{filename}.csv')
+        else: 
+            data.sink_parquet(f'{data_path}/output/{filename}.parquet')
+    elif isinstance(data, pl.DataFrame):
+        if csv == True:
+            data.write_csv(f'{data_path}/output/{filename}.csv')
+        else:
+            data.write_parquet(f'{data_path}/output/{filename}.parquet')
+    elif isinstance(data, np.ndarray):
+        np.save(f'{data_path}/output/{filename}.npy', data)
+    else:
+        raise TypeError('Function doesnt support this data type')
+    
+
+def load_data(filename, data_type, data_path=data_path):
+    ''' 
+    Arguments:
+        filename: 
+            the saved file name
+        data_type: ['lazy', 'np', 'df']
+            the type of data we want to load in
+    '''
+    if data_type == 'lazy':
+        data = pl.scan_parquet(f'{data_path}/output/{filename}.parquet')
+    elif data_type == 'np':
+        data = np.load(f'{data_path}/output/{filename}.npy')
+    elif data_type == 'df':
+        data = pl.read_parquet(f'{data_path}/output/{filename}.parquet')
+    else:
+        raise TypeError('Function doesnt support this data type')
+    return data
+
+def load_json5(filename):
+    with open(f"/home/ma/a/alb25/Project/thesis_code/notebooks/configs/{filename}.json5") as file:
+        return json5.load(file)
